f5369d751e06a767d74c518580ede118fbb8c882
Alexander Suter <alexander.suter@axonivy.com>
2023-02-16 15:50:20 +0100

    Format code

--- src/main/java/ch/ivyteam/ivy/maven/HtmlGenerator.java
+++ src/main/java/ch/ivyteam/ivy/maven/HtmlGenerator.java
@@ -7,22 +7,20 @@ import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.plugin.logging.Log;
 
-public class HtmlGenerator
-{
+public class HtmlGenerator {
 
   private final String templateHtml;
   private final String artifactTargetPath;
   private final List<File> imageFiles;
   private final Path rootDir;
   private final Log log;
-
   private StringBuilder imgTagBuilder;
   private String lastParent;
   private String rootRelativePath;
   private String referenceRelativePath;
 
-  public HtmlGenerator(String template, String artifactTargetPath, List<File> imageFiles, Path rootDir, Log log, String rootRelativePath, String referenceRelativePath)
-  {
+  public HtmlGenerator(String template, String artifactTargetPath, List<File> imageFiles, Path rootDir,
+          Log log, String rootRelativePath, String referenceRelativePath) {
     this.templateHtml = template;
     this.artifactTargetPath = artifactTargetPath;
     this.imageFiles = imageFiles;
@@ -32,32 +30,30 @@ public class HtmlGenerator
     this.log = log;
   }
 
-  public String generate()
-  {
+  public String generate() {
     imgTagBuilder = new StringBuilder();
-    imageFiles.stream().forEach(this::appendImage);   
-    String filledTemplate = StringUtils.replace(templateHtml, GenerateImageHtmlMojo.REPLACE_TAG_IMG, imgTagBuilder.toString());
-    return StringUtils.replace(filledTemplate, GenerateImageHtmlMojo.REPLACE_TAG_TARGET_PATH, artifactTargetPath + referenceRelativePath);
+    imageFiles.stream().forEach(this::appendImage);
+    String filledTemplate = StringUtils.replace(templateHtml, GenerateImageHtmlMojo.REPLACE_TAG_IMG,
+            imgTagBuilder.toString());
+    return StringUtils.replace(filledTemplate, GenerateImageHtmlMojo.REPLACE_TAG_TARGET_PATH,
+            artifactTargetPath + referenceRelativePath);
   }
 
-  private void appendImage(File image)
-  {
+  private void appendImage(File image) {
     Path relativeImagePath = rootDir.relativize(image.toPath());
     appendTitle(relativeImagePath);
-    
     log.debug("Adding: " + relativeImagePath);
-    imgTagBuilder.append("<img src=\"" + rootRelativePath + rootDir.getFileName() + "/" + relativeImagePath + "\" title=\"" + image.getName() + "\">\n");
+    imgTagBuilder.append("<img src=\"" + rootRelativePath + rootDir.getFileName() + "/" + relativeImagePath
+            + "\" title=\"" + image.getName() + "\">\n");
   }
-  
-  private void appendTitle(Path relativeImagePath)
-  {
+
+  private void appendTitle(Path relativeImagePath) {
     String imgParent = relativeImagePath.getParent().toString();
-    if (!StringUtils.equals(imgParent, lastParent))
-    {
+    if (!StringUtils.equals(imgParent, lastParent)) {
       lastParent = imgParent;
       String title = relativeImagePath.getParent().toString();
       String id = StringUtils.replace(title, " ", "-");
-      imgTagBuilder.append("<h3 id='"+id+"'>" + title + "</h3><a href='#"+id+"'>link</a></br>\n");
+      imgTagBuilder.append("<h3 id='" + id + "'>" + title + "</h3><a href='#" + id + "'>link</a></br>\n");
     }
   }
 }
--- src/test/java/ch/ivyteam/ivy/maven/TestImageComparer.java
+++ src/test/java/ch/ivyteam/ivy/maven/TestImageComparer.java
@@ -15,9 +15,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TestImageComparer
-{
-  
+public class TestImageComparer {
+
   private LogCollector log;
   private File refConditionTab;
   private File newConditionTab;
@@ -25,64 +24,55 @@ public class TestImageComparer
   private Path newRoot;
 
   @Before
-  public void setUp() throws IOException
-  {
+  public void setUp() throws IOException {
     log = new LogCollector();
     refRoot = Files.createTempDirectory("refRoot");
     newRoot = Files.createTempDirectory("newRoot");
-    
     refConditionTab = load(refRoot, "refImg/1-Condition Tab.png");
     newConditionTab = load(newRoot, "newImg/1-Condition Tab.png");
   }
-  
+
   @After
-  public void tearDown() throws IOException
-  {
+  public void tearDown() throws IOException {
     FileUtils.deleteDirectory(refRoot.toFile());
     FileUtils.deleteDirectory(newRoot.toFile());
   }
 
   @Test
-  public void compareIdenticalImages()
-  {
+  public void compareIdenticalImages() {
     new ImageComparer(refRoot.toFile(), refRoot.toFile(), asList(refConditionTab), 99.99f, log).compare();
     assertThat(log.getWarnings()).isEmpty();
-    assertThat(log.getDebug().iterator().next().toString()).startsWith("comparing").contains("1-Condition Tab.png");
+    assertThat(log.getDebug().iterator().next().toString()).startsWith("comparing")
+            .contains("1-Condition Tab.png");
   }
-  
+
   @Test
-  public void compareDifferentImages()
-  {
+  public void compareDifferentImages() {
     compare(newConditionTab);
     assertThat(log.getWarnings().iterator().next().toString()).contains("Image only has similarity of");
   }
-  
+
   @Test
-  public void compareDifferentSizedImages() throws IOException
-  {
+  public void compareDifferentSizedImages() throws IOException {
     load(refRoot, "refImg/0-Name Tab.png");
     File newNameTab = load(newRoot, "newImg/0-Name Tab.png");
     compare(newNameTab);
     assertThat(log.getWarnings().iterator().next().toString()).contains("Different sized image:");
   }
-  
+
   @Test
-  public void compareNonExistentImages()
-  {
+  public void compareNonExistentImages() {
     File nonExistentImage = new File(newConditionTab.getParent(), "NonExistentImage.png");
     compare(nonExistentImage);
     assertThat(log.getWarnings().iterator().next().toString()).contains("Could not read image:");
   }
 
-  private void compare(File nonExistentImage)
-  {
+  private void compare(File nonExistentImage) {
     new ImageComparer(refRoot.toFile(), newRoot.toFile(), asList(nonExistentImage), 99.99f, log).compare();
   }
-  
-  private File load(Path rootDir, String testRes) throws IOException
-  {
-    try(InputStream is = TestImageComparer.class.getResourceAsStream("../../../../"+testRes))
-    {
+
+  private File load(Path rootDir, String testRes) throws IOException {
+    try (InputStream is = TestImageComparer.class.getResourceAsStream("../../../../" + testRes)) {
       File elementDir = new File(rootDir.toFile(), "MyElement");
       elementDir.mkdir();
       File img = new File(elementDir, new File(testRes).getName());
@@ -90,5 +80,4 @@ public class TestImageComparer
       return img;
     }
   }
-  
 }
